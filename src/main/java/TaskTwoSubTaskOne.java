dedf4a0e573293815d766164941ce5332b558fb5
agarkov-kv <Unisolll48@yandex.ru>
2024-01-26 18:49:35 +0300

    Task #2.1

---
+++ src/main/java/TaskTwoSubTaskOne.java
@@ -0,0 +1,14 @@
+public class TaskTwoSubTaskOne {
+    public static void main(String[] args) {
+        double a = 13;
+        int b = 6;
+        System.out.println(a - b);
+        System.out.println(a + b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(a * b);
+        System.out.println(a * b);
+        System.out.println(a % 2);
+        System.out.println(b % 2);
+    }
+}
