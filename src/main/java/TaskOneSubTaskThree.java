53148955b8c1ad44cf6f43b79184894eb735aa8e
agarkov-kv <Unisolll48@yandex.ru>
2024-01-26 10:47:53 +0300

    Task #3

--- src/main/java/TaskOneSubTaskThree.java
+++ src/main/java/TaskOneSubTaskThree.java
@@ -1,4 +1,4 @@
-public class Task3 {
+public class TaskOneSubTaskThree {
     public static void main(String[] args) {
         System.out.println(" \uD83E\uDD70");
     }
--- src/main/java/TaskOneSubTaskTwo.java
+++ src/main/java/TaskOneSubTaskTwo.java
@@ -1,4 +1,4 @@
-public class Task2 {
+public class TaskOneSubTaskTwo {
     public static void main(String[] args) {
         System.out.println("""
                 ____888888______________________________
