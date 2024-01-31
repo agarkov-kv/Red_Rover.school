8fd3bd8a6cdc71187e95cd62e3ce6fd577996aba
agarkov-kv <Unisolll48@yandex.ru>
2024-01-31 13:32:54 +0300

    HomeWork 3

---
+++ src/main/java/TaskThreeSubTaskOne.java
@@ -0,0 +1,15 @@
+public class TaskThreeSubTaskOne {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        if (a == b) {
+            System.out.println("Переменные равны");
+        }
+        else if (a < b) {
+            System.out.println("B больше A");
+        }
+        else if (a > b){
+            System.out.println("A больше B");
+        }
+    }
+}
---
+++ src/main/java/TaskThreeSubTaskTwo.java
@@ -0,0 +1,14 @@
+public class TaskThreeSubTaskTwo {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 12;
+        if ((a + b)% 2 == 0) {
+            System.out.println("maybe a and b are even");
+        }
+        else if ((a + b)% 2 != 0) {
+            System.out.println("some variable is odd");
+        }
+
+
+    }
+}
