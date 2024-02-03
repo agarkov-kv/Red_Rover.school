d2d82a33d441be18e50a586d968d353cc6c4b5d4
agarkov-kv <Unisolll48@yandex.ru>
2024-02-03 14:04:01 +0300

    HomeWork 4.1.2

---
+++ src/main/java/TaskFourToOneSubTaskTwo.java
@@ -0,0 +1,7 @@
+public class TaskFourToOneSubTaskTwo {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10000; i = i * 5) {
+            System.out.println(i);
+        }
+    }
+}
