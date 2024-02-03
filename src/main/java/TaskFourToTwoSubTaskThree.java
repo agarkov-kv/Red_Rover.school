60130ba87f5826a13b55bb816893dcd8810c23a0
agarkov-kv <Unisolll48@yandex.ru>
2024-02-03 14:51:37 +0300

    HomeWork 4.2.3

---
+++ src/main/java/TaskFourToTwoSubTaskThree.java
@@ -0,0 +1,8 @@
+public class TaskFourToTwoSubTaskThree {
+    public static void main(String[] args) {
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        for (int i = 0; i < array.length; i++) {
+                System.out.println(array[i] * 15);
+        }
+    }
+}
